e98ef7cd0a1ffb4040f0c4efec0fd127e9ac6314
Ken <kmusante@gmail.com>
2019-03-16 00:57:53 -0400

    Added functioning file drop

---
+++ public/script.js
@@ -0,0 +1,75 @@
+
+function initDropzone() {
+	const dz = document.getElementById('dropzone')
+
+	getInput(dz).addEventListener('change', e => handleChange(e))
+	dz.addEventListener('drop', e => handleDrop(e))
+	dz.addEventListener('dragover', e => handleDragover(e))
+	dz.addEventListener('dragleave', e => handleDragleave(e))
+}
+
+function getInput(dz) {
+	return dz.querySelector('input[type="file"]')
+}
+
+function preventDefault(e) {
+	e.preventDefault()
+	e.stopPropagation()
+}
+
+function handleSelect(e) {
+	processFiles(getDataTransfer(e))
+	preventDefault(e)
+}
+
+function handleChange(e) {
+	handleSelect(e)
+	e.target.value = null
+}
+
+function handleDrop(e) {
+	const dz = e.target
+	const fileInput = getInput(dz)
+	const fileList = getDataTransfer(e)
+
+	if(fileList.length > 0) {
+		handleSelect(e)
+	} else {
+		processFiles(fileList)
+	}
+
+	handleDragleave(e)
+}
+
+function handleDragover(e) {
+	e.target.classList.add('is_dragged-over')
+	preventDefault(e)
+}
+
+function handleDragleave(e) {
+	e.target.classList.remove('is_dragged-over')
+	preventDefault(e)
+}
+
+function getDataTransfer(e) {
+	let items = []
+	if (e.dataTransfer) {
+		const data = e.dataTransfer
+		if (data.files && data.files.length) {
+			items = data.files
+		} else if (data.items && e.target.files) {
+			items = data.items
+		}
+	} else if (e.target && e.target.files) {
+		items = e.target.files
+	}
+	return items
+}
+
+function processFiles(files) {
+	console.log(files)
+}
+
+document.addEventListener('DOMContentLoaded', e => {
+	initDropzone()
+})
